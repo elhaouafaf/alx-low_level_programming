28c5b095b0faf9fd921c728150dea4316cdd9e00
elhaouafaf <elhaou.afaf93@gmail.com>
2023-04-27 01:34:28 +0200

    ag

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
+
 /**
 * print_alphabet - print all alphabet in lowercase
-*
-* Return: Always 0 (Success)
 */
+
 void print_alphabet(void)
 {
-int i;
-for (i = 97; i <= 122; i++)
-{
-_putchar(i);
-}
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 _putchar('\n');
 }
