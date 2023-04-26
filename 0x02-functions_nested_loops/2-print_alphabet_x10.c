ebbde2f6188678821f1cfdd8f41b19d7c268dc38
elhaouafaf <elhaou.afaf93@gmail.com>
2023-04-27 01:43:28 +0200

    ag

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,21 @@
 /**
 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
 * followed by a new line
-* Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
+char ch;
 int i;
-int j;
-for (j = 1; i <= 10; i++)
+i = 0;
+while (i < 10)
 {
-for (j = 97; j <= 122; j++)
+ch = 'a';
+while (ch <= 'z')
 {
-_putchar(j);
+_putchar(ch);
+ch++;
 }
 _putchar('\n');
+i++;
 }
 }
