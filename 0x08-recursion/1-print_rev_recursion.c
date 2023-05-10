ea04aa4b923be6e319841e4b5bca41d4b1df6060
elhaouafaf <elhaou.afaf93@gmail.com>
2023-05-10 14:10:55 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "nain.h"
+/**
+* _print_rev_recursion - Print a String in reverse.
+* @s: The string to be printed .
+*/
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putshar(*s);
+}
+}
