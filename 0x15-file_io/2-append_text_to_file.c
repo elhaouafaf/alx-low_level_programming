60b7417e7a0fec423920e605806a779c74dd0a47
elhaouafaf <elhaou.afaf93@gmail.com>
2023-06-12 16:25:58 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,18 +7,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int of, wri, len = 0;
-if (filename == NULL)
+int fd;
+int len = 0;
+if (!filename)
 return (-1);
-if (text_content != NULL)
-{
-for (len = 0; text_content[len];)
+fd = open(filename, O_RDWR | O_APPEND);
+if (fd == -1)
+return (-1);
+while (text_content && text_content[len])
 len++;
-}
-of = open(filename, O_WRONLY | O_APPEND);
-wri = write(of, text_content, len);
-if (of == -1 || w == -1)
-return (-1);
-close(of);
+write(fd, text_content, len);
+close(fd);
 return (1);
 }
