5d5068ee63797889bb59f26a4dccfa7a6155e6d3
elhaouafaf <elhaou.afaf93@gmail.com>
2023-06-12 16:43:18 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 char *create_buffer(char *file);
 void close_file(int fd);
 /**
@@ -10,72 +8,75 @@ void close_file(int fd);
  */
 char *create_buffer(char *file)
 {
-char *buf;
-buf = malloc(sizeof(char) * 1024);
-if (buf == NULL)
+char *buff;
+buff = malloc(sizeof(char) * 1024);
+if (buff == NULL)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", file);
 exit(99);
 }
-return (buf);
+return (buff);
 }
 /**
- * close_file - kayclosi lfile
- * @fd: to close.
- */
+** close_file - fonction dyal close file
+ * @fd: inpute dyal had void foncction.
+*/
+
 void close_file(int fd)
 {
-int cf;
-cf = close(fd);
-if (cf == -1)
+int cfc;
+
+cfc = close(fd);
+
+if (cfc == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
+
 /**
- * main - kaycopies li fl file.
- * @argc: numb argument.
- * @argv: array lpoint
- * Return: 0 if ok.
- * Description: If the argument count is incorrect exit code 97.
- * ila file_from makanch exit code 98.
- * ila file_to akanch exit code 99.
- * ila file_to or file_from matsedchexit code 100.
- */
+** main - kaycopies li fl file.
+ * @argc: numb arguments.
+ * @argv: array dyal pointer.
+ * Return: ila kolchi ok
+ * Description: ila largument count is ok - exit code 97.
+*/
 int main(int argc, char *argv[])
 {
-int from, to, re, w;
-char *buf;
+int from, to, rrt, w;
+char *buffer;
+
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-buf = create_buffer(argv[2]);
+buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
-re = read(from, buf, 1024);
+rr = read(from, buffer, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
-if (from == -1 || re == -1)
+if (from == -1 || rrt == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't read from file %s\n", argv[1]);
-free(buf);
+free(buffer);
 exit(98);
 }
-w = write(to, buf, re);
+w = write(to, buffer, rrt);
 if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", argv[2]);
-free(buf);
+free(buffer);
 exit(99);
 }
-re = read(from, buf, 1024);
+
+rrt = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
-} while (re > 0);
+} while (rrt > 0);
 free(buffer);
 close_file(from);
 close_file(to);
